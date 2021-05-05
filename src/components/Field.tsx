2e8d7867d86659db54190b5993c74a22b2773dd7
GeneralQuarter <x1q.g1x@gmail.com>
2021-05-06 00:42:42 +0200

    initial commit

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,16 +1,61 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
+import React, { useEffect } from 'react';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
+import { SlugEditor } from '@contentful/field-editor-slug';
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
-const Field = (props: FieldProps) => {
+const Field = ({sdk}: FieldProps) => {
   // If you only want to extend Contentful's default editing experience
   // reuse Contentful's editor components
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+  const commonInfoField = sdk.entry.fields['commonInfo'];
+  const field = sdk.field;
+  const entrySys = sdk.entry.getSys();
+  
+  useEffect(() => {
+    const removeSubscription = commonInfoField.onValueChanged(async value => {
+      if (!value) {
+        if (!entrySys.publishedAt) {
+          field.setValue('');
+        }
+        return;
+      }
+
+      if (field.getValue()) {
+        return;
+      }
+
+      const commonInfo: any = await sdk.space.getEntry(value.sys.id);
+      
+      const codeStart = `${commonInfo.fields.genus.fr[0]}${commonInfo.fields.species.fr[0]}-`.toUpperCase();
+
+      const searchQuery = {
+        content_type: entrySys.contentType.sys.id,
+        'fields.code[match]': codeStart,
+        'sys.id[ne]': entrySys.id,
+        order: '-fields.code',
+        limit: 1
+      }
+
+      const results = await sdk.space.getEntries(searchQuery);
+
+      if (results.total === 0) {
+        field.setValue(codeStart + 1);
+        return;
+      }
+
+      const lastPlant: any = results.items[0];
+      const lastNumber = parseInt(lastPlant.fields.code.fr.split('-')[1]);
+
+      field.setValue(codeStart + (lastNumber + 1));
+    })
+
+    return removeSubscription;
+  });
+
+  return <SlugEditor baseSdk={sdk} field={field} isInitiallyDisabled={true} />
 };
 
 export default Field;
